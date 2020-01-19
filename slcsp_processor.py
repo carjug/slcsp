45cad5f5b71f0897b90a99dec379779091abd1c1
Carly Jugler <carjug@gmail.com>
2020-01-19 13:54:48 -0800

    extract get_rate_for_zip into helper function

--- slcsp_processor.py
+++ slcsp_processor.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import csv
+from os import path
 
-PLANS_CSV = "./plans.csv"
-ZIPS_CSV = "./zips.csv"
-SLCSP_CSV = "./slcsp.csv"
+
+PLANS_CSV = path.join(path.dirname(path.abspath(__file__)), "./plans.csv")
+ZIPS_CSV = path.join(path.dirname(path.abspath(__file__)), "./zips.csv")
+SLCSP_CSV = path.join(path.dirname(path.abspath(__file__)), "./slcsp.csv")
 
 
 def main():
@@ -82,18 +84,38 @@ def get_slcsp_by_zip(silver_plans, zip_rate_areas):
     with open(SLCSP_CSV) as slcsp_file:
         slcsp_reader = csv.DictReader(slcsp_file)
         for row in slcsp_reader:
-            slcsp_rate = ""
-            rate_areas_for_zip = zip_rate_areas.get(row["zipcode"])
-            # if zip has more than one rate area it is ambiguous
-            # and should not be processed
-            if len(rate_areas_for_zip) == 1:
-                rate_area_tuple = rate_areas_for_zip.pop()
-                rates = silver_plans.get(rate_area_tuple[0], {}).get(rate_area_tuple[1])
-                if rates and len(rates) >= 2:
-                    sorted_rates = sorted(rates)
-                    # get the second lowest rate and format it
-                    slcsp_rate = "{0:.2f}".format(float(sorted_rates[1]))
+            zipcode = row["zipcode"]
+            rate_areas_for_zip = zip_rate_areas.get(zipcode)
+            slcsp_rate = get_rate_for_zip(rate_areas_for_zip, silver_plans)
+            print(f"{zipcode},{slcsp_rate}")
 
-            print(f"{row['zipcode']},{slcsp_rate}")
 
-main()
+def get_rate_for_zip(rate_areas_for_zip, silver_plans):
+    """
+    Helper function to get slcsp rate from all the silver plans
+    associated with the passed in rate area
+
+    Returns empty string if there is more than one rate area in the
+    rate_areas_for_zip set
+
+    :param rate_areas_for_zip: set of rate area tuples
+    :param silver_plans: dict of dicts
+    :return: String - empty or formatted rate
+
+    e.g. "234.50"
+    """
+    slcsp_rate = ""
+    # if zip has more than one rate area it is ambiguous
+    # and should not be processed
+    if len(rate_areas_for_zip) == 1:
+        rate_area_tuple = rate_areas_for_zip.pop()
+        rates = silver_plans.get(rate_area_tuple[0], {}).get(rate_area_tuple[1])
+        if rates and len(rates) >= 2:
+            sorted_rates = sorted(rates)
+            # get the second lowest rate and format it
+            slcsp_rate = "{0:.2f}".format(float(sorted_rates[1]))
+    return slcsp_rate
+
+
+if __name__ == "__main__":
+    main()

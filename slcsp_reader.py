b89b181eed5abbb4ce372bf97a6537bafd4e8d87
Carly Jugler <carjug@gmail.com>
2020-01-18 22:44:10 -0800

    import all the necessary files

--- slcsp_reader.py
+++ slcsp_reader.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+
+import csv
+
+PLANS_CSV = "./plans.csv"
+ZIPS_CSV = "./zips.csv"
+SLCSP_CSV = "./slcsp.csv"
+
+
 def main():
     """
     Outer function responsible for executing the subsequent
